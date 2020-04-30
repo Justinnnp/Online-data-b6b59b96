14c213dacff026206393b8bb84b82a9f75ac8f11
Justin Plein <2099449@talnet.nl>
2020-04-30 20:54:13 +0200

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,11 @@
+const car = {
+    naam: "Zara",
+    score: "36",
+    begin_datum: "21 Januari 2016",
+    vrienden: ["Mika", "Josef", "Maria", "Sumail", "Lotte"],
+    gekwalificeerd: true,
+};
+console.log(JSON.stringify(car));
+
+
+
